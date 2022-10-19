c1f20fb81cff78419af4473cb12e83c1cd583ff6
Анастасия <n060600@yandex.ru>
2022-10-19 23:53:01 +0500

    With mongo and models

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
@@ -8,3 +9,6 @@ app.listen(PORT, () => {
     // Если всё работает, консоль покажет, какой порт приложение слушает
     console.log(`App listening on port ${PORT}`)
 })
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+module.exports = mongoose.model('mestodb', mestodb);
