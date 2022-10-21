ab05a66b48b0401121791b211dfac908c33cfb48
Анастасия <n060600@yandex.ru>
2022-10-21 14:42:54 +0500

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -3,20 +3,20 @@ const { errorsCatch } = require('../utils/errors');
 
 module.exports.getUser = (req, res) => {
   User.find({})
-    .then(user => res.send({data: user}))
+    .then(user => res.send({user}))
     .catch((err) => errorsCatch(err))
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then((user) => res.send({data: user}))
+    .then((user) => res.send({user}))
     .catch((err) => errorsCatch(err))
 };
 
 module.exports.getUserId = (req, res) => {
   User.find({})
-    .then(user => res.send({data: user}))
+    .then(user => res.send({user}))
     .catch((err) => errorsCatch(err))
 };
 
