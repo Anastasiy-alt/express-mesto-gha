18edf6a60800299502068040567f993e1eb9cf4b
Анастасия <n060600@yandex.ru>
2022-10-21 14:38:22 +0500

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
-
-
-// module.exports = mongoose.model('mestodb', mestodb);
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
