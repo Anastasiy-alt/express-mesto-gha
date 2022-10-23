f21ad6902e078bb2c0149674008fe1b136f1fd84
Анастасия <n060600@yandex.ru>
2022-10-24 00:43:48 +0500

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -10,7 +10,7 @@ module.exports.getUser = (req, res) => {
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
+  User.create({ name, about, avatar }, { runValidators: true })
     .then((user) => res.send({ data: user }))
     .catch((err) => errorsCatch(err));
 };
