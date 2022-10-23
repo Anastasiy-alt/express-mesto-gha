4b733d9a283d39176129588cf13e1509d9456c5c
Анастасия <n060600@yandex.ru>
2022-10-24 00:45:57 +0500

    Revert "Update users.js"
    This reverts commit f21ad6902e078bb2c0149674008fe1b136f1fd84.

--- controllers/users.js
+++ controllers/users.js
@@ -10,7 +10,7 @@ module.exports.getUser = (req, res) => {
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar }, { runValidators: true })
+  User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
     .catch((err) => errorsCatch(err));
 };
