4a6240696c7415ea0d95a56f5ddfd651f616da7a
Анастасия <n060600@yandex.ru>
2022-10-31 13:44:56 +0500

    2/10

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,4 @@
-/* eslint-disable consistent-return */
+const bcrypt = require('bcryptjs');
 const User = require('../models/users');
 const { errorsCatch, ERROR_NOT_FOUND } = require('../utils/errors');
 
@@ -9,10 +9,21 @@ module.exports.getUser = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+  User.create({
+    name, about, avatar, email,
+  })
     .then((user) => res.send({ data: user }))
     .catch((err) => errorsCatch(err));
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      email: req.body.email,
+      password: hash, // записываем хеш в базу
+    }))
+    .then((user) => res.send(user))
+    .catch((err) => res.status(400).send(err));
 };
 
 module.exports.getUserId = (req, res) => {
--- models/users.js
+++ models/users.js
@@ -1,19 +1,41 @@
+const validator = require('validator');
+
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    // required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
+    // required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: {
+    type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator: (url) => validator.isURL(url),
+      message: 'Неправильный формат ссылки',
+    },
+    // required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: 'Неправильный формат почты',
+    },
+  },
+  password: {
     type: String,
     required: true,
   },
