77f67623056e68f3e7eed13a2efc86702087d1ee
Анастасия <n060600@yandex.ru>
2022-11-01 13:25:09 +0500

    Revert "Update users.js"
    This reverts commit 8a6fb23777185dd1666d4087a07661af1689b5c0.

--- models/users.js
+++ models/users.js
@@ -1,16 +1,18 @@
-// const validator = require('validator');
+const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    // required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
+    // required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Исследователь',
@@ -18,19 +20,20 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-    // validate: {
-    //   validator: (url) => validator.isURL(url),
-    //   message: 'Неправильный формат ссылки',
-    // },
+    validate: {
+      validator: (url) => validator.isURL(url),
+      message: 'Неправильный формат ссылки',
+    },
+    // required: true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
-    // validate: {
-    //   validator: (email) => validator.isEmail(email),
-    //   message: 'Неправильный формат почты',
-    // },
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: 'Неправильный формат почты',
+    },
   },
   password: {
     type: String,
