705be2f3dc35a17b5a685a407c3ae35b802d0fe0
Анастасия <n060600@yandex.ru>
2022-11-03 01:05:07 +0500

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -106,7 +106,7 @@ module.exports.login = (req, res, next) => {
 };
 
 module.exports.getUserMe = (req, res, next) => {
-  const { _id } = req.user._id;
+  const { _id } = req.user;
   User
     .findById(_id)
     .then((user) => {
