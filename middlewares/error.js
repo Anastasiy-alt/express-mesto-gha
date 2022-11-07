6da80e0a86ec6c15f6c990ab7d06625b2d8a513a
Анастасия <n060600@yandex.ru>
2022-11-07 23:20:33 +0500

    Update error.js

--- middlewares/error.js
+++ middlewares/error.js
@@ -3,7 +3,6 @@ const error = (err, req, res, next) => {
   res
     .status(statusCode)
     .send({
-      // проверяем статус и выставляем сообщение в зависимости от него
       message: statusCode === 500
         ? 'На сервере произошла ошибка'
         : message,
